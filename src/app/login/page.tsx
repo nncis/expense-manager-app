63a4b62edacaaf4c5357b0fc2816afb75dff147a
nncis <115885591+nncis@users.noreply.github.com>
2025-03-10 07:20:40 -0300

    feat: add magic link

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,27 @@
 'use client'
 import { signIn } from 'next-auth/react';
+import { useState } from 'react';
 
 export default function LoginPage() {
+
+  const [emailInput, setEmailInput] = useState<string>('');
   
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmailInput(e.target.value)
+  }
+
   return (
     <div>
       <h1>Iniciar Sesión</h1>
-      <button onClick={() => signIn('google', { redirectTo: "dashboard" })}>Iniciar sesión con Google</button>
-      <button onClick={() => signIn('email')}>Iniciar sesión con Magic Link</button>
+      <button onClick={() => signIn('google', { callbackUrl: "/dashboard" })}>Iniciar sesión con Google</button>
+      <p>
+        {emailInput}
+      </p>
+      <input type='email' value={emailInput} onChange={inputHandler}></input>
+      <button onClick={() => {
+        signIn('email', {email: emailInput})
+      }}
+        >Iniciar sesión con Magic Link</button>
     </div>
   );
 }
