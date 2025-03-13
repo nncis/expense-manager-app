e834be046fa6a14ad1504cc3c339d98f5c7811a9
nncis <115885591+nncis@users.noreply.github.com>
2025-03-13 15:45:52 -0300

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -36,7 +36,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: 'lax',
         path: '/',
         secure: process.env.NODE_ENV === 'production', // Usa 'secure' solo en producción
-        domain: 'https://expense-manager-app-topaz.vercel.app/login',
+        domain: '.vercel.app'
       },
     },
   },
