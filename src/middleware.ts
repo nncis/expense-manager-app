4ff508071c5996374e8a5faa44f9e5fec62c2464
nncis <115885591+nncis@users.noreply.github.com>
2025-03-13 20:20:10 -0300

    depure middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,21 @@ import { NextResponse, type NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(request: NextRequest) {
-  
   // Obtén las cookies del request
   const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+  console.log('Token obtenido:', token);
   // Define las rutas protegidas
   const protectedRoutes = ['/dashboard'];
   const isProtectedRoute = protectedRoutes.some((route) =>
     request.nextUrl.pathname.startsWith(route),
-  );
-
-  // Si la ruta es protegida y el usuario no está autenticado, redirige al login
-  if (isProtectedRoute && !token) {
-    console.log('Usuario no autenticado, redirigiendo al login...');
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
+);
+console.log('Ruta solicitada:', request.nextUrl.pathname);
+console.log('Es ruta protegida:', isProtectedRoute);
+// Si la ruta es protegida y el usuario no está autenticado, redirige al login
+if (isProtectedRoute && !token) {
+  console.log('Usuario no autenticado, redirigiendo al login...');
+  return NextResponse.redirect(new URL('/login', request.url));
+}
 
   // Permitir el acceso si el usuario está autenticado o la ruta no es protegida
   return NextResponse.next();
