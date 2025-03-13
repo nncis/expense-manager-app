ae52acd825348b6ade8394ba889248654d51c4c6
nncis <115885591+nncis@users.noreply.github.com>
2025-03-13 13:07:32 -0300

    fix: responsive

--- src/app/dashboard/record/page.tsx
+++ src/app/dashboard/record/page.tsx
@@ -1,8 +1,9 @@
 import RecordForm from '@/ui/record/recordForm';
+import style from '@/styles/record.module.css'
 
 export default function Record(){
   return (
-    <main>
+    <main className={style.recordMainPage}>
       <RecordForm />
     </main>
   )
