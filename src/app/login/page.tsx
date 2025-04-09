f5ab7741abb69f1af81332ab88f6125f0bb09ba7
nncis <115885591+nncis@users.noreply.github.com>
2025-04-09 09:27:14 -0300

    feat: add login page style

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -15,22 +15,32 @@ export default function LoginPage() {
 
   return (
     <main className={style.loginPage}>
-      <h1 className={style.mainTitle}>Welcome</h1>
-        <h4></h4>
-        <input placeholder='Enter your email' className={style.loginInput} type='email' value={emailInput} onChange={inputHandler}></input>
-        <button onClick={() => {
-          signIn('email', {email: emailInput})
-        }}
-        className={style.submitBtn}
-          >Log in</button>
-          <p>Or</p>
+      <h1 className={style.mainTitle}>Welcome to Expense Manager</h1>
+
+      {/* google button */}
         <button 
           onClick={() => signIn('google', { callbackUrl: "/dashboard" })}
           className={style.googleBtn}
           >
-          
           <SiGoogle className={style.googleIcon} />
+          Continue with Google
         </button>
+      {/* ------------   */}
+        <div className={style.divisor}>
+        <div className={style.divisorLine}></div>
+        <p>OR</p>
+        <div className={style.divisorLine}></div>
+        </div>
+      {/* email button */}
+        <input placeholder='Email' className={style.loginInput} type='email' value={emailInput} onChange={inputHandler}></input>
+        <button onClick={() => {
+          signIn('email', {email: emailInput})
+          }}
+          className={style.submitBtn}
+          >
+          Continue with Email
+        </button>
+      {/* ---------- */}
     </main>
   );
 }
