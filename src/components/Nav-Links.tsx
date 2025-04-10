a8870f90c9b1d020242e94a6c85f15420f333e34
nncis <115885591+nncis@users.noreply.github.com>
2025-04-10 06:31:51 -0300

    feat: style nav bar

--- src/components/Nav-Links.tsx
+++ src/components/Nav-Links.tsx
@@ -3,13 +3,13 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import clsx from 'clsx';
 import style from '@/styles/nav.module.css'
-import { PencilSquareIcon, BookOpenIcon, ChartBarIcon } from '@heroicons/react/24/outline';
+import { PencilSquareIcon, BookOpenIcon, ChartBarIcon, PlusIcon } from '@heroicons/react/24/outline';
 
 const links = [
   {
     name: 'Record',
     href: '/dashboard/record',
-    icon: PencilSquareIcon
+    icon: PlusIcon
   },
   { 
     name: 'Expenses', 
@@ -40,7 +40,7 @@ export default function NavLinks() {
           )}
         >
           {<link.icon className={style.icon}/>}
-          <p className={style.navTitle}>{link.name}</p>
+
         </Link>
       )
     })}
--- src/ui/Nav/Navbar.tsx
+++ src/ui/Nav/Navbar.tsx
@@ -7,24 +7,24 @@ import { PowerIcon,  } from '@heroicons/react/24/outline';
 export default function SideNav() {
   return (
     <div className={style.nav}>
-      <div className={style.logo}>
-      </div>
+      {/* <div className={style.logo}>
+      </div> */}
       <nav className={style.sidenav}>
         <NavLinks />
       </nav>
       <div
         className={style.signOutContainer}
       >
-      <div className={style.blankSpace}>
-      </div>
-        <button 
+      {/* <div className={style.blankSpace}>
+      </div> */}
+        {/* <button 
           className={style.signOut}
           onClick={()=> signOut({callbackUrl: '/'})}
           type='button'
           >
           <PowerIcon width={30}/>
           <p>Sign Out</p>
-        </button>
+        </button> */}
       </div>
     </div>
   )
