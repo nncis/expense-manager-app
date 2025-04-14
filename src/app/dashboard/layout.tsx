740b4eb55a84aa6c64beb69d2d10856ec63fb3b8
nncis <115885591+nncis@users.noreply.github.com>
2025-04-14 07:48:55 -0300

    fix: real height mobile

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,12 @@
+'use client'
 import style from '@/styles/dashboard.module.css';
 import Navbar from "@/ui/Nav/Navbar";
+import { useViewportHeight } from "@/lib/hooks"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  useViewportHeight()
   return (
-      <div className={style.dashboardContainer}>
+      <div className={`${style.dashboardContainer} full-height`}>
           <Navbar/>
           {children}
       </div>
