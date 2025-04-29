c2d39f14ed3067a17db34892b9e83ca089a67887
nncis <115885591+nncis@users.noreply.github.com>
2025-04-29 08:03:58 -0300

    fix

--- src/app/api/resume/monthly-expenses/route.ts
+++ src/app/api/resume/monthly-expenses/route.ts
@@ -1,15 +1,15 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { expenseByMonth } from '@/lib/data'
+import { NextRequest, NextResponse } from "next/server";
+import { getExpenseByMonth } from '@/lib/data'
 
 
-// export async function GET(req: NextRequest) {
-//   const { searchParams } = new URL(req.url);
-//   const date = searchParams.get("month");
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const date = searchParams.get("month");
 
-//   if(date){
-//     const data = await expenseByMonth(date);
-//     return NextResponse.json(data)
-//   } else {
-//     return NextResponse.json({ error: "Month required" }, { status: 400 });
-//   }
-// };
+  if(date){
+    const data = await getExpenseByMonth(date);
+    return NextResponse.json(data)
+  } else {
+    return NextResponse.json({ error: "Month required" }, { status: 400 });
+  }
+};
