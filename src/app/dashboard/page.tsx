f6a015a35181996018da515309e286575455d65e
nncis <115885591+nncis@users.noreply.github.com>
2025-06-12 07:07:59 -0300

    feat: redirect with tokken

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,15 @@
-export default function Dashboard(){
+import { getServerSession } from "next-auth";
+import { authOptions } from '@/lib/auth';
+import { redirect } from 'next/navigation';
+
+export default async function Dashboard(){
+
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect('/dashboard/record');
+  }
+
   return (
     <main>
       Dashboard
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
-
 import style from "./page.module.css";
 import Link from "next/link";
 
-export default function Home() {
+import { getServerSession } from "next-auth";
+import { authOptions } from '@/lib/auth';
+import { redirect } from 'next/navigation';
+
+
+export default async function Home() {
+
+    const session = await getServerSession(authOptions);
+
+    if (session) {
+      redirect('/dashboard/record');
+    }
+
   return (
     <main className={style.getStartedPage}>
       <h1 className={style.getStartedTitle}>Expense Manager</h1>
